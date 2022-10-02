9efe356b70975826ccb1de016e1d09edb0332179
Jeff Schwab <jeff@unbuggy.com>
2022-10-02 09:30:15 -0400

    Solve problem 1

---
+++ src/bin/euler1.rs
@@ -0,0 +1,22 @@
+//! If we list all the natural numbers below 10 that are multiples of 3 or 5,
+//! we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//!
+//! Find the sum of all the multiples of 3 or 5 below 1000.
+
+fn euler1() -> u64 {
+    (1..1000).filter(|n| n % 3 == 0 || n % 5 == 0).sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(euler1(), 233168);
+    }
+}
+
+fn main() {
+    println!("{}", euler1());
+}
