56c5ba34986cf00abc02d15b3617b5802462e501
Jeff Schwab <jeff@unbuggy.com>
2022-10-02 14:13:54 -0400

    Solve problem 10

---
+++ src/bin/euler10.rs
@@ -0,0 +1,32 @@
+//! The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//!
+//! Find the sum of all the primes below two million.
+
+use rust_euler::Primes;
+
+fn sum_primes_below(limit: u64) -> u64 {
+    Primes::new().take_while(|&p| p < limit).sum()
+}
+
+fn euler() -> u64 {
+    sum_primes_below(2_000_000)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_sum_primes_below() {
+        assert_eq!(sum_primes_below(10), 17);
+    }
+
+    #[test]
+    fn test_euler() {
+        assert_eq!(euler(), todo!());
+    }
+}
+
+fn main() {
+    println!("{}", euler());
+}
